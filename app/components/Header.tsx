0c5d366c8d5030e822885c9255d607b023d05ae7
Kaleem Shaikh <kaleem.shaikh@lr.org>
2024-11-02 03:08:45 +0530

    Installation of antdesign, version fix, and Create simple header Comp

---
+++ app/components/Header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <header className="bg-white shadow">
+      <div className="max-w-7xl mx-auto py-6 px-4">
+        <h1 className="text-3xl font-bold text-orange-400">APCO </h1>
+      </div>
+    </header>
+  );
+};
